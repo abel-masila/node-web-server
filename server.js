d59d65616ee63c472923393c64b850a3aca7ca99
masila <abelmasila@gmail.com>
2018-04-24 11:38:30 +0300

    Added afew routes

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express= require('express');
 const app= new express();
 
 app.get('/',(req, resp)=>{
-    resp.send('Hello express');
+    //resp.send('<h1>Hello express</h1>');
+    resp.send({
+        name:'Abel Masila',
+        age: 25,
+        likes:[
+            'biking','swimmin'
+        ]
+    })
+});
+
+app.get('/about',(req,resp)=>{
+    resp.send('About Page');
+});
+app.get('/bad',(req,resp)=>{
+    resp.send({
+        errorMessage:'Something went bad!'
+    });
 });
 
 app.listen(3000);
