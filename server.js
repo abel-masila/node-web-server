ba88ba9a0581441f13d1588626338834b6cd8020
masila <abelmasila@gmail.com>
2018-04-24 12:03:45 +0300

    Added hbs templates

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express= require('express');
+const hbs=require('hbs');
 
 const app= new express();
 
 
+app.set('view engine', 'hbs');
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',(req, resp)=>{
-    //resp.send('<h1>Hello express</h1>');
-    resp.send({
-        name:'Abel Masila',
-        age: 25,
-        likes:[
-            'biking','swimmin'
-        ]
+    resp.render('home',{
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my website'
     })
 });
 
 app.get('/about',(req,resp)=>{
-    resp.send('About Page');
+    resp.render('about',{
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 app.get('/bad',(req,resp)=>{
     resp.send({
